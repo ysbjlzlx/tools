ac59ce351a9ba3fe0d46baf3c28451d5dfc49cff
where.liu <where.liu@huolala.cn>
2021-01-22 20:00:30 +0800

    feat: json

--- src/views/Json.vue
+++ src/views/Json.vue
@@ -1,25 +1,34 @@
 <template>
     <div>
-        <div id="jsoneditor" style="width: 400px; height: 400px"></div>
+        <div ref="jsoneditor" id="jsoneditor" style="width: 100%; height: 800px"></div>
     </div>
 </template>
 <script setup>
+import { onMounted, reactive, ref } from "vue";
 import JSONEditor from "jsoneditor";
+import "jsoneditor/dist/jsoneditor.min.css";
 
-console.log(JSONEditor);
-// create the editor
-const container = document.getElementById("jsoneditor");
-const options = {};
-const editor = new JSONEditor(container, options);
+const state = reactive({ plain: null });
+const jsoneditor = ref(null);
+onMounted(() => {
+    // console.log(jsoneditor);
+    // create the editor
+    const container = document.getElementById("jsoneditor");
+    const options = {
+        language: "zh-CN",
+        mode: "code",
+    };
+    const editor = new JSONEditor(jsoneditor.value, options);
 
-// set json
-const initialJson = {
-    Array: [1, 2, 3],
-    Boolean: true,
-    Null: null,
-    Number: 123,
-    Object: { a: "b", c: "d" },
-    String: "Hello World",
-};
-editor.set(initialJson);
+    // set json
+    const initialJson = {
+        Array: [1, 2, 3],
+        Boolean: true,
+        Null: null,
+        Number: 123,
+        Object: { a: "b", c: "d" },
+        String: "Hello World",
+    };
+    editor.set(initialJson);
+});
 </script>
