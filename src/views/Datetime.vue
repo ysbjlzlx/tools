aba2d205100be392d42cafce76da3aab368fa6ea
liuzhaowei <liuzhaowei55@gmail.com>
2021-01-23 23:41:56 +0800

    feat: datetime

--- src/views/Datetime.vue
+++ src/views/Datetime.vue
@@ -1,9 +1,35 @@
 <template>
-    <div></div>
+  <div>
+    <div class="row">
+      <div class="col-12 mt-0 col-md-6 mt-md-0 form-floating">
+        <input type="text" class="form-control" v-model="state.timestamp" placeholder="时间戳" />
+        <label>时间戳</label>
+      </div>
+      <div class="col-12 mt-1 col-md-6 mt-md-0 form-floating">
+        <input type="text" class="form-control" v-model="state.datetime" placeholder="时间" />
+        <label>时间</label>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
-import { reactive } from "vue";
+import { onMounted, reactive, watch } from "vue";
+import { getUnixTime, format, fromUnixTime } from "date-fns";
+
 const state = reactive({
-    datetime: null,
+  timestamp: null,
+  datetime: null,
+});
+watch(
+  () => state.timestamp,
+  (val) => {
+    const date = fromUnixTime(Number(val));
+    state.datetime = format(date, "uuuu-MM-dd HH:mm:ss");
+  }
+);
+onMounted(() => {
+  const date = new Date();
+  state.timestamp = getUnixTime(date);
+  state.datetime = format(date, "uuuu-MM-dd HH:mm:ss");
 });
 </script>
--- src/views/layouts/Nav.vue
+++ src/views/layouts/Nav.vue
@@ -1,34 +1,34 @@
 <template>
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/">Tools</a>
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/hash">Hash</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/color">Color</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/json">JSON</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/datetime">Datetime</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">Tools</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="/color">Color</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="/datetime">Datetime</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="/hash">Hash</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="/json">JSON</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
