16961e5c5406940e2dfbbab52afc17442f298137
liuzhaowei <liuzhaowei55@gmail.com>
2021-01-25 23:40:21 +0800

    feat: url

--- src/views/encode/URLEncode.vue
+++ src/views/encode/URLEncode.vue
@@ -5,8 +5,10 @@
       <label>原始 URL</label>
     </div>
     <div class="border rounded mt-2">
-      <button type="button" class="btn btn-primary" v-on:click="encode"><i class="bi bi-arrow-down"></i> 编码</button>
-      <button type="button" class="btn btn-primary" v-on:click="decode"><i class="bi bi-arrow-up"></i>还原</button>
+      <div class="btn-group" role="group">
+        <button type="button" class="btn btn-outline-primary" v-on:click="encode"><i class="bi bi-arrow-down"></i> 编码</button>
+        <button type="button" class="btn btn-outline-primary" v-on:click="decode"><i class="bi bi-arrow-up"></i>还原</button>
+      </div>
     </div>
     <div class="form-floating mt-2">
       <textarea class="form-control" style="height: 100px" v-model="state.encodeURI" placeholder="encodeURI"></textarea>
