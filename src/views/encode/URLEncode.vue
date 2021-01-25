8a5ff9ce98085775df2db3ac551f46fec8d01388
liuzhaowei <liuzhaowei55@gmail.com>
2021-01-25 23:32:24 +0800

    feat: textarea

--- src/views/encode/URLEncode.vue
+++ src/views/encode/URLEncode.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="form-floating">
-      <input type="text" class="form-control" v-model="state.plain" placeholder="原始 URL" />
+      <textarea class="form-control" style="height: 100px" v-model="state.plain" placeholder="原始 URI"></textarea>
       <label>原始 URL</label>
     </div>
     <div class="border rounded mt-2">
@@ -9,11 +9,11 @@
       <button type="button" class="btn btn-primary" v-on:click="decode"><i class="bi bi-arrow-up"></i>还原</button>
     </div>
     <div class="form-floating mt-2">
-      <input type="text" class="form-control" v-model="state.encodeURI" placeholder="encodeURI" />
+      <textarea class="form-control" style="height: 100px" v-model="state.encodeURI" placeholder="encodeURI"></textarea>
       <label>encodeURI</label>
     </div>
     <div class="form-floating mt-1">
-      <input type="text" class="form-control" v-model="state.encodeURIComponent" placeholder="encodeURIComponent" />
+      <textarea class="form-control" style="height: 100px" v-model="state.encodeURIComponent" placeholder="encodeURIComponent"></textarea>
       <label>encodeURIComponent</label>
     </div>
   </div>
