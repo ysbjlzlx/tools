ae328d02e4b8ee0a3a8e7deed614d09c13697f4b
where.liu <where.liu@huolala.cn>
2021-01-26 08:57:29 +0800

    feat: base64

--- src/views/encode/Base64.vue
+++ src/views/encode/Base64.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="row mt-5">
-    <div class="col-5">
-      <label class="form-label">原始文本</label>
-      <textarea class="form-control" rows="16" v-model="state.plain" placeholder="原始文本"></textarea>
+  <div class="mt-1">
+    <div class="form-floating">
+      <textarea class="form-control" style="height: 100px" v-model="state.plain" placeholder="原始文本"></textarea>
+      <label>原始文本</label>
     </div>
-    <div class="col-2">
-      <div>
-        <button type="button" class="btn btn-primary" v-on:click="base64Encode">编码 <i class="bi bi-arrow-right" aria-hidden="true"></i></button>
-      </div>
-      <div class="mt-1">
-        <button type="button" class="btn btn-primary" v-on:click="base64Decode"><i class="bi bi-arrow-left" aria-hidden="true"></i> 解码</button>
+    <div class="border rounded mt-2">
+      <div class="btn-group">
+        <button type="button" class="btn btn-outline-primary" v-on:click="base64Encode">编码 <i class="bi bi-arrow-down" aria-hidden="true"></i></button>
+        <button type="button" class="btn btn-outline-primary" v-on:click="base64Decode"><i class="bi bi-arrow-up" aria-hidden="true"></i> 解码</button>
       </div>
     </div>
-    <div class="col-5">
-      <label class="form-label">Base64 文本</label>
-      <textarea class="form-control" rows="16" v-model="state.encoded"></textarea>
+    <div class="form-floating mt-2">
+      <textarea class="form-control" style="height: 100px" v-model="state.encoded" placeholder="Base64 文本"></textarea>
+      <label>Base64 文本</label>
     </div>
   </div>
 </template>
