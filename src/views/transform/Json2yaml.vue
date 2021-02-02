f4c68b3b9d6a8bbdd479b8b2a3b6cb0c27d4e9ec
where.liu <where.liu@huolala.cn>
2021-02-02 13:03:50 +0800

    feat: json2yaml

--- src/views/transform/Json2yaml.vue
+++ src/views/transform/Json2yaml.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="mt-2">
-    <div></div>
-    <div class="row">
+    <div class="btn-group">
+      <button role="button" class="btn btn-outline-primary" v-on:click="json2yaml()">JSON <i class="bi bi-arrow-right"></i> YAML</button>
+      <button role="button" class="btn btn-outline-primary" v-on:click="yaml2json()">YAML <i class="bi bi-arrow-right"></i> JSON</button>
+    </div>
+    <div class="row mt-2">
       <div class="col form-floating">
         <textarea class="form-control" style="height: 512px" v-model="state.json" placeholder="JSON"></textarea>
         <label>JSON</label>
@@ -17,7 +20,7 @@
 <script setup>
 import Toast from "/@/src/components/Toast.vue";
 import { reactive, watch } from "vue";
-import * as YAML from "yaml";
+import YAML from "yaml";
 
 const state = reactive({
   json: "",
@@ -28,21 +31,32 @@ const state = reactive({
     show: false,
   },
 });
-watch(
-  () => state.json,
-  (val) => {
-    try {
-      const json = JSON.parse(val);
-      state.yaml = YAML.stringify(json);
-    } catch (e) {
-      if (e instanceof SyntaxError) {
-        state.toast.title = "Tools";
-        state.toast.content = "JSON 格式错误";
-        state.toast.show = true;
-      } else {
-        console.warn(e);
-      }
+const json2yaml = () => {
+  try {
+    const json = JSON.parse(state.json);
+    state.yaml = YAML.stringify(json);
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      state.toast.title = "Tools";
+      state.toast.content = "JSON 格式错误";
+      state.toast.show = true;
+    } else {
+      console.warn(e);
     }
   }
-);
+};
+const yaml2json = () => {
+  try {
+    const yaml = YAML.parse(state.yaml);
+    state.json = JSON.stringify(yaml);
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      state.toast.title = "Tools";
+      state.toast.content = "JSON 格式错误";
+      state.toast.show = true;
+    } else {
+      console.warn(e);
+    }
+  }
+};
 </script>
