4655971203c30f3e70b31b31f2ca308cdf5b8f72
where.liu <where.liu@huolala.cn>
2021-02-04 09:37:22 +0800

    feat: icon

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import i18n from "./locales";
 import "element-plus/lib/theme-chalk/index.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const app = createApp(App);
 app.use(i18n);
