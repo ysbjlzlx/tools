df6cea0ff7f6269e4565d2fee2b0a114b21e54ec
where.liu <where.liu@huolala.cn>
2021-02-05 09:31:03 +0800

    feat: add global main.css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import i18n from "./locales";
+import "./main.css";
 import "element-plus/lib/theme-chalk/index.css";
 
 const app = createApp(App);
