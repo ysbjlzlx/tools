cc7d21914f2e1689dc8962733efac380d135117e
liuzhaowei <liuzhaowei55@gmail.com>
2021-02-10 19:08:09 +0800

    feat: util

--- src/scripts/helper/util.js
+++ src/scripts/helper/util.js
@@ -71,3 +71,20 @@ window.location.platform = function () {
   }
   return platform;
 };
+/**
+ * 随机打乱数组
+ *
+ * @return {Array}
+ */
+Array.prototype.shuffle = function () {
+  var input = this;
+
+  for (var i = input.length - 1; i >= 0; i--) {
+    var randomIndex = Math.floor(Math.random() * (i + 1));
+    var itemAtIndex = input[randomIndex];
+
+    input[randomIndex] = input[i];
+    input[i] = itemAtIndex;
+  }
+  return input;
+};
