d426966f2e0ffadc6c8f7d5105509a68953688c4
where.liu <where.liu@huolala.cn>
2021-04-22 16:42:52 +0800

    feat: hash

--- src/views/Hash.vue
+++ src/views/Hash.vue
@@ -3,41 +3,27 @@
     <label>{{ t("common.text_plain") }}</label>
     <el-input v-model="state.plain" :placeholder="t('common.text_plain')" />
   </div>
-  <div></div>
-  <el-block show-margin>
-    <label>MD5</label>
-    <el-input v-model="state.plain" :placeholder="t('common.text_plain')" disabled />
-  </el-block>
-
-  <div>
-    <div class="form-floating">
-      <input class="form-control" type="text" v-model="state.plain" :placeholder="t('common.text_plain')" />
-      <label>{{ t("common.text_plain") }}</label>
-    </div>
-    <div class="mt-2 form-floating">
-      <input class="form-control" type="text" :value="getMd5()" disabled placeholder="MD5" />
-      <label>MD5</label>
-    </div>
-    <div class="mt-2 form-floating">
-      <input class="form-control" type="text" :value="getSha1()" disabled placeholder="SHA1" />
-      <label>SHA1</label>
-    </div>
-    <div class="mt-2 form-floating">
-      <input class="form-control" type="text" :value="getSha256()" disabled placeholder="SHA256" />
-      <label>SHA256</label>
-    </div>
-    <div class="mt-2 form-floating">
-      <input class="form-control" type="text" :value="getSha512()" disabled placeholder="SHA512" />
-      <label>SHA512</label>
-    </div>
-  </div>
+  <el-form label-position="right" label-width="60px" style="margin-top: 15px">
+    <el-form-item label="MD5">
+      <el-input :value="getMd5()" placeholder="MD5" disabled />
+    </el-form-item>
+    <el-form-item label="SHA1">
+      <el-input :value="getSha1()" disabled placeholder="SHA1" />
+    </el-form-item>
+    <el-form-item label="SHA256">
+      <el-input :value="getSha256()" disabled placeholder="SHA256" />
+    </el-form-item>
+    <el-form-item label="SHA512">
+      <el-input :value="getSha512()" disabled placeholder="SHA512" />
+    </el-form-item>
+  </el-form>
 </template>
 
 <script setup>
 import { reactive } from "vue";
 import { MD5, SHA1, SHA256, SHA512 } from "crypto-js";
 import { useI18n } from "vue-i18n";
-import { ElInput } from "element-plus";
+import { ElInput, ElForm, ElFormItem } from "element-plus";
 import ElBlock from "/@/src/components/ElBlock.vue";
 
 const { t } = useI18n({ useScope: "global" });
