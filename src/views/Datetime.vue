e457d8d3834bd5db7c2806c19975547c2ea448c3
where.liu <where.liu@huolala.cn>
2021-04-22 14:01:02 +0800

    feat: datetime

--- src/views/Datetime.vue
+++ src/views/Datetime.vue
@@ -1,31 +1,30 @@
 <template>
-  <div>
-    <div class="row mt-2">
-      <div class="col-12 mt-0 col-md-6 mt-md-0 form-floating">
-        <input type="text" class="form-control" v-model="state.timestamp" :placeholder="t('datetime.timestamp')" />
-        <label>{{ t("datetime.timestamp") }}</label>
-      </div>
-      <div class="col-12 mt-1 col-md-6 mt-md-0 form-floating">
-        <input type="text" class="form-control" v-bind:value="getDatetime()" :placeholder="t('datetime.datetime')" />
-        <label>{{ t("datetime.datetime") }}</label>
-      </div>
-    </div>
-    <div class="row mt-2">
-      <div class="col-12 mt-1 col-md-6 mt-md-0 form-floating">
-        <input type="text" class="form-control" v-model="state.datetime" :placeholder="t('datetime.datetime')" />
-        <label>{{ t("datetime.datetime") }}</label>
-      </div>
-      <div class="col-12 mt-0 col-md-6 mt-md-0 form-floating">
-        <input type="text" class="form-control" v-bind:value="getTimestamp()" :placeholder="t('datetime.timestamp')" />
-        <label>{{ t("datetime.timestamp") }}</label>
-      </div>
-    </div>
-  </div>
+  <el-row>
+    <el-col :span="12">
+      <label>{{ t("datetime.timestamp") }}</label>
+      <el-input v-model="state.timestamp" :placeholder="t('datetime.timestamp')" />
+    </el-col>
+    <el-col :span="12">
+      <label>{{ t("datetime.datetime") }}</label>
+      <el-input v-bind:value="getDatetime()" :placeholder="t('datetime.datetime')" />
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="12">
+      <label>{{ t("datetime.datetime") }}</label>
+      <el-input v-model="state.datetime" :placeholder="t('datetime.datetime')" />
+    </el-col>
+    <el-col :span="12">
+      <label>{{ t("datetime.timestamp") }}</label>
+      <el-input v-bind:value="getTimestamp()" :placeholder="t('datetime.timestamp')" />
+    </el-col>
+  </el-row>
 </template>
 <script setup>
 import { onMounted, reactive } from "vue";
 import { getUnixTime, format, fromUnixTime, parse } from "date-fns";
 import { useI18n } from "vue-i18n";
+import { ElRow, ElCol, ElInput, ElSpace } from "element-plus";
 
 const { t } = useI18n({ useScope: "global" });
 
