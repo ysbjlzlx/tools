1fd56fb65cac99c019abb7196fccc2031dd4b554
where.liu <where.liu@huolala.cn>
2021-05-06 15:38:27 +0800

    feat: save

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <el-container style="min-height: 100vh">
-    <el-header>
+    <el-header style="padding: 0">
       <Nav></Nav>
     </el-header>
     <el-main style="flex: 1">
       <router-view></router-view>
     </el-main>
-    <el-footer>
+    <el-footer style="padding: 0">
       <Footer></Footer>
     </el-footer>
   </el-container>
 </template>
 <script setup>
-import { ElContainer, ElHeader, ElMain, ElFooter } from "element-plus";
 import Nav from "./views/layouts/Nav.vue";
 import Footer from "./views/layouts/Footer.vue";
 </script>
