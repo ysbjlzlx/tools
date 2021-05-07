c4ec22ec44cebc403069f9c2b2724499607c16fc
where.liu <where.liu@huolala.cn>
2021-05-07 13:42:36 +0800

    feat: save

--- src/App.vue
+++ src/App.vue
@@ -11,24 +11,21 @@
     </el-footer>
   </el-container>
 </template>
-<script setup>
+<script>
 import Nav from "./views/layouts/Nav.vue";
 import Footer from "./views/layouts/Footer.vue";
+export default {
+  components: { Nav, Footer },
+};
 </script>
 
 <style>
-#app {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
-    "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-  font-size: 14px;
-  line-height: 22px;
-  font-weight: 400;
-  color: #000000da;
-  min-height: 100vh;
-}
 body {
   display: flex;
   flex-flow: column;
+  min-height: 100vh;
+}
+#app {
   min-height: 100vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import ElementPlus from "element-plus";
 import App from "./App.vue";
 import router from "./router";
 import i18n from "./locales";
-import "./main.css";
-import 'nprogress/nprogress.css'
 import "element-plus/lib/theme-chalk/index.css";
+import "nprogress/nprogress.css";
+import "./main.css";
 
 const app = createApp(App);
 app.use(ElementPlus, { size: "small" });
