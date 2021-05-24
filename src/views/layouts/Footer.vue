12c115a00a0733438cdf20444b09dede9ecdf0a8
where.liu <where.liu@huolala.cn>
2021-05-24 17:56:15 +0800

    feat: save

--- src/views/layouts/Footer.vue
+++ src/views/layouts/Footer.vue
@@ -2,13 +2,7 @@
   <div class="container text-center mt-5">
     <ul>
       <li>
-        <a href="https://github.com/ysbjlzlx/tools" target="_blank">GitHub</a>
-      </li>
-      <li>
-        <span id="busuanzi_container_site_pv">本站总访问量 <span id="busuanzi_value_site_pv">0</span> 次</span>
-      </li>
-      <li>
-        <span id="busuanzi_container_page_pv">本文总阅读量 <span id="busuanzi_value_page_pv">0</span> 次 </span>
+        <el-link type="info" href="https://github.com/ysbjlzlx/tools" target="_blank">GitHub</el-link>
       </li>
     </ul>
   </div>
