e84840c27abc1f220fb5ebe4a40ad203ed446187
where.liu <where.liu@huolala.cn>
2021-08-04 13:25:19 +0800

    feat: save

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,70 +2,70 @@ const routes = [
   {
     path: "/",
     name: "Index",
-    component: () => import("../views/Index.vue"),
+    component: () => import("../pages/Index.vue"),
   },
   {
     path: "/hash",
     name: "Hash",
-    component: () => import("../views/Hash.vue"),
+    component: () => import("../pages/Hash.vue"),
   },
   {
     path: "/color",
     name: "Color",
-    component: () => import("../views/Color.vue"),
+    component: () => import("../pages/Color.vue"),
   },
   {
     path: "/json",
     name: "Json",
-    component: () => import("../views/Json.vue"),
+    component: () => import("../pages/Json.vue"),
   },
   {
     path: "/datetime",
     name: "Datetime",
-    component: () => import("../views/Datetime.vue"),
+    component: () => import("../pages/Datetime.vue"),
   },
   {
     path: "/encode/base64",
     name: "Base64",
-    component: () => import("../views/encode/Base64.vue"),
+    component: () => import("../pages/encode/Base64.vue"),
   },
   {
     path: "/encode/urlencode",
     name: "URLEncode",
-    component: () => import("../views/encode/URLEncode.vue"),
+    component: () => import("../pages/encode/URLEncode.vue"),
   },
   {
     path: "/image/placeholder",
     name: "ImagePlaceholder",
-    component: () => import("../views/image/Placeholder.vue"),
+    component: () => import("../pages/image/Placeholder.vue"),
   },
   {
     path: "/image/qrcode",
     name: "ImageQRCode",
-    component: () => import("../views/image/QRCode.vue"),
+    component: () => import("../pages/image/QRCode.vue"),
   },
   {
     path: "/transform/json2yaml",
     name: "Json2yaml",
-    component: () => import("../views/transform/Json2yaml.vue"),
+    component: () => import("../pages/transform/Json2yaml.vue"),
   },
   {
     path: "/password/make",
     name: "PasswordMake",
-    component: () => import("../views/password/make.vue"),
+    component: () => import("../pages/password/make.vue"),
   },
   {
     path: "/password/otp",
     name: "PasswordOtp",
-    component: () => import("../views/Otp.vue"),
+    component: () => import("../pages/Otp.vue"),
   },
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
-  },
+  // {
+  //   path: '/',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [
+  //     { path: '', component: () => import('pages/Index.vue') }
+  //   ]
+  // },
 
   // Always leave this as last one,
   // but you can also remove it
